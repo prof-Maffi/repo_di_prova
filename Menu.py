0f94284597819b5e3d32f84c71d50b6c6edc9d0a
ANDREA MAFFI <andrea.maffi@itisdalmine.edu.it>
2024-12-17 10:01:15 +0100

    implementazione del menu

--- Menu.py
+++ Menu.py
@@ -1,1 +1,14 @@
-print("Hello")
+import calcolatrice as clc
+
+while True:    
+    print("Benvenuto nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
+    if scelta==0:
+        break
+    elif scelta==1:
+        clc.Sottrazione()
+    elif scelta==2:
+        clc.Somma()
+    else:
+        print("Scelta non corretta!")
--- calcolatrice.py
+++ calcolatrice.py
@@ -9,17 +9,3 @@ def Sottrazione():
     n_2=int(input("inserisci il secondo numero: "))
     risultato = n_1 - n_2
     print(f"{n_1} - {n_2} = {risultato}")
-
-while True:    
-    print("Benvenuto nella nostra calcolatrice")
-    print("Inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
-    if scelta==0:
-        break
-    elif scelta==1:
-        Sottrazione()
-    elif scelta==2:
-        Somma()
-    else:
-        print("Scelta non corretta!")
-
