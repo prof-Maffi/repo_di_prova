acac378fc3166b8383d71a8d0fd9ea8b5c67407c
prof-Maffi <132429799+prof-Maffi@users.noreply.github.com>
2024-12-17 09:56:25 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,1 +1,12 @@
-print("Hello")
+while True:    
+    print("Benvenuto nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
+    if scelta==0:
+        break
+    elif scelta==1:
+        Sottrazione()
+    elif scelta==2:
+        Somma()
+    else:
+        print("Scelta non corretta!")
