a5bf6a8774b06cea8b65c418de09c883e52ccb94
prof-Maffi <132429799+prof-Maffi@users.noreply.github.com>
2024-12-17 09:56:56 +0100

    delete menu part

--- calcolatrice.py
+++ calcolatrice.py
@@ -10,16 +10,4 @@ def Sottrazione():
     risultato = n_1 - n_2
     print(f"{n_1} - {n_2} = {risultato}")
 
-while True:    
-    print("Benvenuto nella nostra calcolatrice")
-    print("Inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
-    if scelta==0:
-        break
-    elif scelta==1:
-        Sottrazione()
-    elif scelta==2:
-        Somma()
-    else:
-        print("Scelta non corretta!")
 
